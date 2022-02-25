b80a22f28ad2cb21cfef5bb3737619e670914311
avindhawale <avin.dhawale@gmail.com>
2022-02-25 09:56:01 +0530

    fixed scroll position issue

--- views/questions/questions.js
+++ views/questions/questions.js
@@ -1,7 +1,7 @@
 let currentQuestion = 1;
 
 const moveToNextStep = () => {
-  document.getElementById("question" + currentQuestion).scrollIntoView();
+  document.getElementById("page" + currentQuestion).scrollIntoView(false);
 };
 
 //this function is being called from timer.js file in onTimesUp function
